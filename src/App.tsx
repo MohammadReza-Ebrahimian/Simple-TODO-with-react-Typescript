b38bab69f27d630026d6e978d2a34384892225a5
Mohammad Reza <131019677+Monsieurreza@users.noreply.github.com>
2023-12-21 18:15:03 +0330

    Getting User Input with ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./app.css";
+import NewTodo from "./components/NewTodo";
 import TodoList from "./components/TodoList";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
 
   return (
     <div>
+      <NewTodo/>
       <TodoList items={todos}/>
     </div>
   );
